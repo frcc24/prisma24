8a67a8c66b70e8646ec2185ec0af1e5f1a18022b
Franco Correa <frcc24@gmail.com>
2025-06-06 10:45:17 -0300

    fix: correct syntax error in FutureBuilder widget

--- lib/presentation/pages/general_pages/full_mode_page.dart
+++ lib/presentation/pages/general_pages/full_mode_page.dart
@@ -178,7 +178,7 @@ class _FullModePageState extends State<FullModePage> {
                 );
               },
               ),
-            ];
+            ],);
           },
         ),
       ),
